1f983b3cbdb4c6a0d2b3196da4a26f4c850b3fbd
Justin Yoo <justin.yoo@aliencube.com>
2015-11-08 21:20:52 +1100

    Add Angular directive templates

--- src/EventSourcingCqrsSample.WebApp/App/models/registrationModel.ts
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="../../Scripts/typings/angularjs/angular.d.ts" />
-
-"use strict";
-
-module app.angular.Models {
-    export class RegistrationModel {
-        streamId: string;
-    }
-}
